5f1d71d6d4b407e1823a1e0197da9ca8133ad2a1
wkoslik <wkoslik@gmail.com>
2021-02-11 17:58:32 -0500

    adding links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
+
+import Header from './partials/Header';
+import Content from './pages/Content';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Content />
+        </main>
+      </div>
+    </Router>
   );
 }
 
--- src/pages/Content.js
+++ src/pages/Content.js
@@ -1,7 +1,14 @@
+import { Route } from 'react-router-dom'
+import Home from './Home'
+import Bounties from './Bounties'
+import NewBounty from './NewBounty'
+
 const Content = (props) => {
     return (
         <div>
-            <h2>content</h2>
+            <Route exact path='/' component={Home} />
+            <Route path ='/bounties' component={Bounties} />
+            <Route path='/bounties/new' component={NewBounty} />
         </div>
     )
 }
